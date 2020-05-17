db548dd6f3a2a96eefe0132eb76ff00467e0b1e7
PolishCodeGirl <malachowska.marzena@gmail.com>
2020-05-17 20:58:11 +0200

    Add hero image and load transformed images

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,23 @@ import usePosts from '../hooks/usePosts'
 
 import Layout from '../components/layout'
 import PostPreview from '../components/PostPreview'
+import Hero from '../components/Hero'
 
 export default () => {
     const posts = usePosts();
 
     return (
-        <Layout>
-            <h1>Home Page</h1>
-            <p>Hi Marzena!</p>
-            <Link to='/about/'>Go to about me &rarr;</Link>
-            
-            <h2>Read my posts</h2>
-            {posts.map(post => (
-                <PostPreview post={post} />
-            ))}
-        </Layout>)
+        <>
+            <Hero />
+            <Layout>
+                <h1>Home Page</h1>
+                <p>Hi Marzena!</p>
+                <Link to='/about/'>Go to about me &rarr;</Link>
+
+                <h2>Read my posts</h2>
+                {posts.map(post => (
+                    <PostPreview post={post} />
+                ))}
+            </Layout>
+        </>)
 }
