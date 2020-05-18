288cead3fcaa1bf18d1d2a4570602052585b4345
PolishCodeGirl <malachowska.marzena@gmail.com>
2020-05-18 20:19:19 +0200

    Render images in mdx

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -1,27 +1,45 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { Link } from 'gatsby'
+import Image from 'gatsby-image'
 
 import ReadLink from './ReadLink'
 
 const PostPreview = ({ post }) => (
     <Article>
-        <h3>
-            <Link to={post.slug}>{post.title}</Link>
-        </h3>
-        <p>{post.excerpt}</p>
-        <ReadLink to={post.slug}>read this post &rarr;</ReadLink>
+        <ImageLink to={post.slug}>
+            <StyledImage fluid={post.image.sharp.fluid} alt={post.alt} />
+        </ImageLink>
+        <div>
+            <h3>
+                <Link to={post.slug}>{post.title}</Link>
+            </h3>
+            <p>{post.excerpt}</p>
+            <ReadLink to={post.slug}>read this post &rarr;</ReadLink>
+        </div>
     </Article>
 )
 
 export default PostPreview
 
 const Article = styled.article`
+    display: flex;
      border-bottom: 1px solid #ddd;
-      margin-top: 0.75rem;
+      margin-top: 0;
       padding-bottom: 1rem;
 
       :first-of-type {
         margin-top: 1rem;
       }
+`
+
+const ImageLink = styled(Link)`
+    margin: 1rem 1rem 0 0;
+    width: 100px;
+`
+
+const StyledImage = styled(Image)`
+    * {
+        margin-top: 0;
+    }
 `
--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -3,23 +3,37 @@ import { graphql, useStaticQuery } from 'gatsby';
 const usePosts = () => {
     const data = useStaticQuery(graphql`
     query {
-        allMdx {
-          nodes {
-            frontmatter {
-              title
-              author
-              slug
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            author
+            slug
+            alt
+            image {
+              sharp: childImageSharp {
+                fluid(
+                  maxWidth: 100
+                  maxHeight: 100
+                  duotone: { shadow: "#663399", highlight: "#ddbbff" }
+                ) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
             }
-            excerpt
           }
+          excerpt
         }
       }
+    }
     `);
   
     return data.allMdx.nodes.map(post => ({
         title: post.frontmatter.title,
         author: post.frontmatter.author,
         slug: post.frontmatter.slug,
+        alt: post.frontmatter.alt,
+        image: post.frontmatter.image,
         excerpt: post.excerpt,
     }))
   };
